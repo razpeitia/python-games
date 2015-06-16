9eac5ba487f2638a76be29cbf8cc78b0fd91b3dd
Ricardo Azpeitia Pimentel <razpeitia@gmail.com>
2015-06-15 23:03:34 -0500

    Initial commit

---
+++ snake/snake01.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+
+# Special code to center window
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+def main():
+    # Settings
+    FRAMES_PER_SECOND = 30
+    RESOLUTION = WIDTH, HEGIHT = 800, 600
+    WINDOWS_TITLE = 'Snake v1.0 by raz'
+    
+    # Initial Setup
+    pygame.init()
+    screen_surface = pygame.display.set_mode(RESOLUTION)
+    pygame.display.set_caption(WINDOWS_TITLE)
+
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        # Delays until next frame
+        clock.tick(FRAMES_PER_SECOND)
+    pygame.quit()
+
+main()
---
+++ snake/snake03.py
@@ -0,0 +1,65 @@
+import pygame
+import os
+
+# Special code to center window
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+
+class Snake(object):
+    def __init__(self):
+        self.snake = [(World.GRID_W/2-1, World.GRID_H/2), (World.GRID_W/2, World.GRID_H/2)]
+        self.dx = 0.1
+        self.dy = 0.0
+
+    def draw(self, screen):
+        block_size = World.BLOCK_SIZE
+        block_color = World.WHITE
+        for x, y in self.snake:
+            block_pos = (x * World.BLOCK_W, y * World.BLOCK_H)
+            rect = pygame.Rect(block_pos, block_size)
+            pygame.draw.rect(screen, block_color, rect)
+
+    def update(self):
+        pass
+
+
+class World(object):
+    FRAMES_PER_SECOND = 30
+    RESOLUTION = WIDTH, HEGIHT = 800, 600
+    GRID_SIZE = GRID_W, GRID_H = (40, 40)
+    BLOCK_SIZE = BLOCK_W, BLOCK_H = (WIDTH / GRID_W, HEGIHT / GRID_H)
+    WINDOWS_TITLE = 'Snake v1.0 by raz'
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+
+    def __init__(self):
+        self.player = Snake()
+
+    def draw(self, screen):
+        screen.fill(self.BLACK)
+        self.player.draw(screen)
+        pygame.display.flip()
+
+    def update(self):
+        pass
+
+
+def main():
+    # Initial Setup
+    pygame.init()
+    screen_surface = pygame.display.set_mode(World.RESOLUTION)
+    pygame.display.set_caption(World.WINDOWS_TITLE)
+
+    world = World()
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        world.draw(screen_surface)
+        # Delays until next frame
+        clock.tick(World.FRAMES_PER_SECOND)
+    pygame.quit()
+
+main()
